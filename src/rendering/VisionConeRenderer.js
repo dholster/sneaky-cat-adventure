86baf8c4b0a8827876f2331166b9c15adbf7ae57
Vibecode Terminal <terminal@vibecodeapp.com>
2025-11-11 18:31:55 +0000

    Sandbox autosave

--- src/rendering/VisionConeRenderer.js
+++ src/rendering/VisionConeRenderer.js
@@ -48,7 +48,7 @@ export class VisionConeRenderer {
 
     const cone = new THREE.Mesh(geometry, material)
     cone.position.z = 0.5 // In front of ground, behind entities
-    cone.rotation.z = Math.PI / 2 // Rotate to face right initially
+    cone.rotation.z = 0 // Start pointing right (matches detection system angle 0)
 
     // Make sure vision cones don't interact with raycasting or collision
     cone.userData.isVisionCone = true // Tag for exclusion
